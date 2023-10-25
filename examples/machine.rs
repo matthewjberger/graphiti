d12eafa587cbd127f0576091e6c45396a91bfbc1
Matthew J. Berger <matthewjordanberger@gmail.com>
2023-10-25 16:04:16 -0600

    SAVEPOINT

--- examples/machine.rs
+++ examples/machine.rs
@@ -4,14 +4,14 @@ trait MachineModule {
 }
 
 struct LightModule {
-    id: u32,
+    id: String,
     brightness: u8,
     color: String,
 }
 
 impl MachineModule for LightModule {
     fn name(&self) -> String {
-        format!("LightModule_{}", self.id)
+        format!("Light {}", self.id)
     }
 
     fn details(&self) -> String {
@@ -20,101 +20,175 @@ impl MachineModule for LightModule {
 }
 
 struct HvacModule {
-    id: u32,
+    id: String,
     temperature: f32,
     fan_speed: u8,
 }
 
 impl MachineModule for HvacModule {
     fn name(&self) -> String {
-        format!("HvacModule_{}", self.id)
+        format!("HVAC {}", self.id)
     }
 
     fn details(&self) -> String {
         format!(
-            "Temperature: {}°C, Fan Speed: {}",
+            "Temperature: {}°C, Fan Speed: {}%",
             self.temperature, self.fan_speed
         )
     }
 }
 
-macro_rules! machine_description {
+// New additions for config.json representation
+trait ConfigModule {
+    fn identity(&self) -> (String, String);
+    fn sections(&self) -> String;
+}
+
+struct ZoneModule {
+    owner: String,
+    subject: String,
+    sections: String,
+}
+
+impl ConfigModule for ZoneModule {
+    fn identity(&self) -> (String, String) {
+        (self.owner.clone(), self.subject.clone())
+    }
+
+    fn sections(&self) -> String {
+        self.sections.clone()
+    }
+}
+
+struct LightConfigModule {
+    owner: String,
+    subject: String,
+    zones: Vec<u32>,
+}
+
+impl ConfigModule for LightConfigModule {
+    fn identity(&self) -> (String, String) {
+        (self.owner.clone(), self.subject.clone())
+    }
+
+    fn sections(&self) -> String {
+        format!("{:?}", self.zones)
+    }
+}
+
+struct ConfigDescriptionBuilder {
+    builder: graphiti::DescriptionBuilder,
+    module_mapping: std::collections::HashMap<String, Vec<String>>,
+}
+
+impl ConfigDescriptionBuilder {
+    fn new() -> Self {
+        Self {
+            builder: graphiti::DescriptionBuilder::new(),
+            module_mapping: std::collections::HashMap::new(),
+        }
+    }
+
+    fn add_zone(&mut self, zone: &str) -> Result<(), graphiti::Error> {
+        self.builder
+            .add_node(zone.to_string(), ("Zone".to_string(),))
+            .map(|_| ())
+    }
+
+    fn add_module<T: ConfigModule>(
+        &mut self,
+        module_type: &str,
+        module_instance: &T,
+    ) -> Result<(), graphiti::Error> {
+        let (owner, subject) = module_instance.identity();
+        let name = format!("{}_{}", owner, subject);
+        self.module_mapping
+            .entry(module_type.to_string())
+            .or_insert(Vec::new())
+            .push(name.clone());
+        self.builder
+            .add_node(name, (module_instance.sections(),))
+            .map(|_| ())
+    }
+
+    fn add_connection(&mut self, zone: &str, module_type: &str) -> Result<(), graphiti::Error> {
+        let source_name = zone.to_string();
+        if let Some(targets) = self.module_mapping.get(module_type) {
+            for target_name in targets {
+                self.builder
+                    .add_edge("Contains", &source_name, vec![target_name])?;
+            }
+        }
+        Ok(())
+    }
+
+    fn build(self) -> Result<graphiti::Description, graphiti::Error> {
+        Ok(self.builder.build())
+    }
+}
+
+macro_rules! config_description {
     (
-        groups: [$($group:ident),*],
-        devices: {$($device:ident: [$($modules:expr),* $(,)?]),* $(,)?},
-        connections: {$($conn_group:ident: [$($conn_device:ident),* $(,)?]),* $(,)?}
+        zones: [$($zone:ident),*],
+        modules: {$($module_type:ident: [$($module_instance:expr),* $(,)?]),* $(,)?},
+        connections: {$($zone_conn:ident: [$($module_conn:ident),* $(,)?]),* $(,)?}
     ) => {{
         (|| -> Result<graphiti::Description, graphiti::Error> {
-            let mut builder = graphiti::DescriptionBuilder::new();
-            let mut device_mapping: std::collections::HashMap<String, Vec<String>> = std::collections::HashMap::new();
+            let mut config_builder = ConfigDescriptionBuilder::new();
 
-            // Adding nodes for groups
+            // Adding nodes for zones
             $(
-                builder.add_node(stringify!($group).to_string(), ("Group".to_string(),))?;
+                config_builder.add_zone(stringify!($zone))?;
             )*
 
-            // Adding nodes for devices and maintaining a mapping of device identifier to module names
+            // Adding nodes for modules
             $(
-                let mut module_names = Vec::new();
                 $(
-                    let name = $modules.name();
-                    module_names.push(name.clone());
-                    builder.add_node(name, ($modules.details(),))?;
+                    config_builder.add_module(stringify!($module_type), &$module_instance)?;
                 )*
-                device_mapping.insert(stringify!($device).to_string(), module_names);
             )*
 
             // Adding edges using the correct node names
             $(
-                let source_name = stringify!($conn_group).to_string();
                 $(
-                    let targets = device_mapping.get(&stringify!($conn_device).to_string()).unwrap();
-                    for target_name in targets {
-                        builder.add_edge("Contains", &source_name, vec![target_name])?;
-                    }
+                    config_builder.add_connection(stringify!($zone_conn), stringify!($module_conn))?;
                 )*
             )*
 
-            Ok(builder.build())
+            config_builder.build()
         })()
     }};
 }
 
-fn main() {
-    let light_module_1 = LightModule {
-        id: 1,
-        brightness: 80,
-        color: "White".to_string(),
-    };
-    let hvac_module_1 = HvacModule {
-        id: 1,
-        temperature: 22.5,
-        fan_speed: 3,
-    };
-    let hvac_module_2 = HvacModule {
-        id: 2,
-        temperature: 20.0,
-        fan_speed: 2,
+fn main() -> Result<(), graphiti::Error> {
+    // Example data resembling the structure from config.json
+    let zone1 = ZoneModule {
+        owner: "owner1".to_string(),
+        subject: "subject1".to_string(),
+        sections: "some_section_data".to_string(),
     };
 
-    let description_result = machine_description! {
-        groups: [group_1, group_2],
-        devices: {
-            device_1: [light_module_1, hvac_module_1],
-            device_2: [hvac_module_2]
+    let light_module1 = LightConfigModule {
+        owner: "owner1".to_string(),
+        subject: "subject1".to_string(),
+        zones: vec![1, 2, 3],
+    };
+
+    // Using the config_description! macro to build a Description
+    let description = config_description! {
+        zones: [zoneA, zoneB],
+        modules: {
+            ZoneModule: [zone1],
+            LightConfigModule: [light_module1]
         },
         connections: {
-            group_1: [device_1],
-            group_2: [device_1, device_2]
+            zoneA: [ZoneModule, LightConfigModule],
+            zoneB: [LightConfigModule]
         }
-    };
+    }?;
 
-    match description_result {
-        Ok(description) => {
-            println!("{:#?}", description);
-        }
-        Err(error) => {
-            eprintln!("Error: {:?}", error);
-        }
-    }
+    // Print or process the description as needed
+    println!("{:?}", description);
+
+    Ok(())
 }
